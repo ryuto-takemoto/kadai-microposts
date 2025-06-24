6fc8b2e55d7aa71e941b6c6baa22d9742ce91edf
ryuto.takemoto <ryuto.takemoto@hakuhodody-one.co.jp>
2025-06-24 12:09:04 +0900

    micropost_wide

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,37 +5,8 @@
         @if (Auth::check())
             <div class="grid grid-cols-3 gap-4">
                 {{-- 左部レーン --}}
-                <div class="col-span-1 flex flex-col h-screen items-start"> {{-- items-start を追加 --}}
-                    {{-- ロゴ --}}
-                    <div class="flex justify-start items-start"> {{-- justify-start を追加 --}}
-                        <h1><a class="btn btn-ghost normal-case text-xl" href="/">Microposts</a></h1>
-                    </div>
-
-                    {{-- メニュー --}}
-                    <div class="flex flex-col items-start justify-center flex-grow"> {{-- items-start を追加 --}}
-                        <a href="{{ route('dashboard') }}" class="btn btn-ghost normal-case">トップページ</a>
-                        <a href="{{ route('users.show', Auth::id()) }}" class="btn btn-ghost normal-case">マイページ</a>
-                        <a href="{{ route('search.index') }}" class="btn btn-ghost normal-case">検索ページ</a>
-                        <a href="{{ route('users.index') }}" class="btn btn-ghost normal-case">ユーザー一覧</a>
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <button type="submit" class="btn btn-ghost normal-case">ログアウト</button>
-                        </form>
-                    </div>
-
-                    {{-- ユーザー情報 --}}
-                    <div class="flex flex-col items-start justify-end"> {{-- items-start を追加 --}}
-                        <a href="{{ route('users.show', Auth::id()) }}">
-                            <div class="avatar mb-2">
-                                <div class="w-12 rounded">
-                                    <img src="{{ Gravatar::get(Auth::user()->email) }}" alt="avatar" />
-                                </div>
-                            </div>
-                        </a>
-                        <p class="text-left">{{ Auth::user()->name }}</p> {{-- text-left を追加 --}}
-                    </div>
-                </div>
-
+                @include('layouts.sidebar') {{-- サイドバーの共通化 --}}
+                
                 {{-- 中央レーン --}}
                 <div class="col-span-1 flex flex-col">
                     {{-- タイムライン --}}
